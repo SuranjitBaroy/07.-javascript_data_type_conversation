92068a5c91c0f5f1421cd60dfcdb695125ea315a
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-19 16:37:50 +0600

    Add files via upload
    JS_data_type_conversation

---
+++ learn.js
@@ -0,0 +1,39 @@
+//Data Type Method 2 Class-4
+
+typeof(24); // eta console er moddhe check korte hobe
+
+//Type Converstion Class-4 Number to String Convert
+var num = String(24); // eta ekti function 
+
+num =(24).toString();
+console.log(typeof(num));
+
+//Type Converstion Class-4 Number to String Convert
+var num1 = Number("24");
+var num1 = Number("This is a text");
+
+console.log(num1);
+console.log(typeof(num1));
+
+//Boolean to string
+var theBul = Number(false);
+
+console.log(theBul);
+console.log(typeof(theBul));
+
+//Date is a object
+var theDate =String(new Date());
+var theDate =(new Date()).toString();
+
+console.log(theDate);
+console.log(typeof(theDate));
+
+//Array to String
+var arr = ([1,55,66]).toString();
+var arr = String([1,55,66]);
+
+console.log(arr);
+console.log(typeof(arr));
+
+
+
